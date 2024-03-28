e5478b863b4f3a0124c5e0eaa9e1c2e11edfc2f5
Mário Guilherme <marioquilherme991@gmail.com>
2024-03-27 22:38:57 -0300

    Adicionando funcionalidade de passar valores por parametros na URL, página de bicicletas e página de seguros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Ativar Links do Menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +12,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
